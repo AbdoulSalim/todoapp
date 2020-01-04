0014ab070fa07825ff30b7a3fa2954170408178e
Abdoul Salim <gabdoulsalim@live.fr>
2020-01-04 16:05:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Todos from './elements/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: "Learn React"},
+      {id: 2, content: "Then Redux"}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    let todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    })
+    this.setState({
+      todos: todos
+    })
+  }
+  
+  render() {
+    return(
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
