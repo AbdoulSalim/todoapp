96fd82671408fa4c20a7eb8e65df65feed152613
Abdoul Salim <gabdoulsalim@live.fr>
2020-01-16 13:23:23 +0100

    adding another stuff

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React, {Component} from 'react';
-import Todos from './elements/Todos';
-import AddTodo from './elements/AddTodo';
+import React, { Component } from "react";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 class App extends Component {
   state = {
     todos: [
-      {id: 1, content: "Learn React"},
-      {id: 2, content: "Then Redux"}
+      { id: 1, content: "Learn React" },
+      { id: 2, content: "Then Redux" },
+      { id: 3, content: "Practice practice practice!" },
+      { id: 4, content: "Then learn GastBy and buid my portfolio by the way" },
+      { id: 5, content: "Find a job!" }
     ]
-  }
+  };
 
-  deleteTodo = (id) => {
-    let todos = this.state.todos.filter((todo) => {
+  deleteTodo = id => {
+    let todos = this.state.todos.filter(todo => {
       return todo.id !== id;
-    })
+    });
     this.setState({
       todos: todos
-    })
-  }
+    });
+  };
 
-  addTodo = (todo) => {
+  addTodo = todo => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
     this.setState({
       todos: todos
-    })
-  }
+    });
+  };
 
   render() {
-    return(
+    return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,23 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Todos = ({ todos, deleteTodo }) => {
-
   const todoList = todos.length ? (
     todos.map(todo => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span onClick={() => deleteTodo(todo.id)}>{ todo.content }</span>
+          <span onClick={() => deleteTodo(todo.id)}>{todo.content}</span>
         </div>
-      )
+      );
     })
   ) : (
-    <p className='center'>There is nothing to do again !</p>
-  )
-  return (
-    <div className="todos collection">
-      {todoList}
-    </div>
-  )
-}
+    <p className="center">There is nothing to do again !</p>
+  );
+  return <div className="todos collection">{todoList}</div>;
+};
 
 export default Todos;
